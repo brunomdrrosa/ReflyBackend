32d75707e8f8105af2627f9a17724a03958c9cf0
brunomdr <brunomdr46@gmail.com>
2022-09-16 08:26:14 -0300

    Primeiro commit

---
+++ src/main/java/com/cesuca/refly/ReflyApplication.java
@@ -0,0 +1,13 @@
+package com.cesuca.refly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReflyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReflyApplication.class, args);
+	}
+
+}
